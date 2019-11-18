5cf8cf965d56bfede3c1234a3d2468d0691b1c58
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-11-18 18:45:08 +0900

    Update card processing.c

--- card processing.c
+++ card processing.c
@@ -7,4 +7,166 @@ int getCardNum(int cardnum) {
 //print the card information (e.g. DiaA)
 void printCard(int cardnum) {
 	
+	int num =0;
+	
+	if( num ==1)
+		{printf("CLV A") };
+		
+	else if (num==2)
+		{prinft("CLV2")	};
+	
+	else if (num==3)
+		{prinft("CLV3")	};
+		
+	else if (num==4)
+		{prinft("CLV4")	};
+		
+	else if (num==5)
+		{prinft("CLV5")	};
+		
+	else if (num==6)
+		{prinft("CLV6")	};	
+		
+ 	else if (num==7)
+		{prinft("CLV7")	};
+		
+	else if (num==8)
+		{prinft("CLV8")	};
+		
+	else if (num==9)
+		{prinft("CLV9")	};
+		
+	else if (num==10)
+		{prinft("CLV10") };
+		
+	else if (num==11)
+		{prinft("CLV J") };
+		
+	else if (num==12)
+		{prinft("CLV Q") };
+		
+	else if (num==13)
+		{prinft("CLV K") };
+				
+	else if( num ==14)
+		{printf("HRT A") };
+		
+	else if (num==15)
+		{prinft("HRT2")	};
+	
+	else if (num==16)
+		{prinft("HRT3")	};
+		
+	else if (num==17)
+		{prinft("HRT4")	};
+		
+	else if (num==18)
+		{prinft("HRT5")	};
+		
+	else if (num==19)
+		{prinft("HRT6")	};	
+		
+ 	else if (num==20)
+		{prinft("HRT7")	};
+		
+	else if (num==21)
+		{prinft("HRT8")	};
+		
+	else if (num==22)
+		{prinft("HRT9")	};
+		
+	else if (num==23)
+		{prinft("HRT10") };
+		
+	else if (num==24)
+		{prinft("HRT J") };
+		
+	else if (num==25)
+		{prinft("HRT Q") };
+		
+	else if (num==26)
+		{prinft("HRT K") };
+		
+	else if( num ==27)
+		{printf("SPD A") };
+		
+	else if (num==28)
+		{prinft("SPD2")	};
+	
+	else if (num==29)
+		{prinft("SPD3")	};
+		
+	else if (num==30)
+		{prinft("SPD4")	};
+		
+	else if (num==31)
+		{prinft("SPD5")	};
+		
+	else if (num==32)
+		{prinft("SPD6")	};	
+		
+ 	else if (num==33)
+		{prinft("SPD7")	};
+		
+	else if (num==34)
+		{prinft("SPD8")	};
+		
+	else if (num==35)
+		{prinft("SPD9")	};
+		
+	else if (num==36)
+		{prinft("SPD10") };
+		
+	else if (num==37)
+		{prinft("SPD J") };
+		
+	else if (num==38)
+		{prinft("SPD Q") };
+		
+	else if (num==39)
+		{prinft("SPD K") };
+		
+	else if( num ==40)
+		{printf("DIA A") };
+		
+	else if (num==41)
+		{prinft("DIA2")	};
+	
+	else if (num==42)
+		{prinft("DIA3")	};
+		
+	else if (num==43)
+		{prinft("DIA4")	};
+		
+	else if (num==44)
+		{prinft("DIA5")	};
+		
+	else if (num==45)
+		{prinft("DIA6")	};	
+		
+ 	else if (num==46)
+		{prinft("DIA7")	};
+		
+	else if (num==47)
+		{prinft("DIA8")	};
+		
+	else if (num==48)
+		{prinft("DIA9")	};
+		
+	else if (num==49)
+		{prinft("DIA10") };
+		
+	else if (num==50)
+		{prinft("DIA J") };
+		
+	else if (num==51)
+		{prinft("DIA Q") };
+		
+	else if (num==52)
+		{prinft("DIA K") };
+		
+
 }
+
+
+
