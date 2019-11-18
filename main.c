18bf2a42193e4202113db26c9bf6fb3fade8f7b7
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-11-18 17:44:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
+#define N_CARDSET			1
+#define N_CARD				52
 #define N_DOLLAR			50
 
 
@@ -77,20 +76,7 @@ int pullCard(void) {
 
 //player settiing
 int configUser(void) {
-	int num_player;
-
-printf("input the number of players (max 5) : ");
-scanf("%d",&num_player);
-
-if(num_player>6)
-	do
-	{
-		printf("Too many players!\ninput the number of players (max 5) : ");
-		scanf("%d",&num_player);
-	}while(num_player<6);
 	
-else if(num_player<6)
-	return num_player;
 }
 
 
