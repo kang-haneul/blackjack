f917174baf10d58e7ee8f72e0dc79d69ca263055
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-11-21 01:57:02 +0900

    Update really betDollar.c

--- really betDollar.c
+++ really betDollar.c
@@ -1,8 +1,8 @@
 int betDollar (void)
 {
-
-
 int betting_Dollar;
+int i;
+int otherplayers_bettingmoney=0;
 
 printf("-----BETTING STEP-----\n");
 
@@ -19,5 +19,12 @@ if(betting_Dollar>N_DOLLAR+1)
 	
 else if (betting_Dollar<N_DOLLAR+1)
 	return betting_Dollar;
+
+for(i=1;i<num_player=1;i++)	
+	{
+	  otherplayers_bettingmoney=1+rand()%N_MAX_BET;
+	printf("-> player %d bets $%d (out of $50): ",i,otherplayers_bettingmoney);
 	
+return 0;
+
 }
